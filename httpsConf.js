fd220814648c3ffe06a98162e35bda5becfcf22d
Carlos Felipe <develop.krlosflip@gmail.com>
2020-05-15 02:14:43 -0500

    ga

--- httpsConf.js
+++ httpsConf.js
@@ -1,8 +1,6 @@
 // WebXR requires HTTPS, so the site doesn't work if someone manually enters
 // the URL and ends up using HTTP. To work around this, force redirect from
 // http to https for non-localhost addresses.
-window = global['window'];
-
 if (window.location.protocol == "http:" &&
     window.location.hostname != "localhost" && 
     window.location.hostname != "127.0.0.1" &&
